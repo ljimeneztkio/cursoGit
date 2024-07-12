6537b5bdea67854c0a954369e460db0fa203f025
LuisJimenez <ljimenezt@kio.tech>
2024-07-12 12:44:08 -0600

    Se agrega la estructura de HTML 5 basica y se imprime Saludo de la empresa con PHP

--- prueba.php
+++ prueba.php
@@ -1,3 +1,32 @@
-<?php
-	echo "Hola a todos desde PHP";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<header>
+		<hgroup>
+			<h1>Encabezado de pagina web</h1>
+			<h2>Subtitulo de la pagina</h2>
+		</hgroup>
+		<nav>
+			<ul>
+				<li><a href="Opcion1.php">Opcion 1</a></li>
+				<li><a href="Opcion2.php">Opcion 2</a></li>
+				<li><a href="Opcion3.php">Opcion 3</a></li>
+				<li><a href="Opcion4.php">Opcion 4</a></li>
+				<li><a href="Opcion5.php">Opcion 5</a></li>
+			</ul>
+		</nav>
+	</header>
+	<section>
+		<div>
+			<?= "Bienvenido a la página web de esta empresa" ?>
+		</div>
+	</section>
+	<aside></aside>
+	<footer></footer>
+</body>
+</html>
