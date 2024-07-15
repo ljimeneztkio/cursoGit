2a86a296f9def8b77cc606bf65274a2b0691fe45
LuisJimenez <ljimenezt@kio.tech>
2024-07-15 17:54:54 -0600

    Creacion de la opcion2 del menu

---
+++ option2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main/main.css">
+    <title>Opcion 2</title>
+</head>
+<body>
+    <header>
+        <nav></nav>
+    </header>
+    <section>
+        <div>
+            <?= "Bienvenido a la opción 2"; ?>
+        </div>
+    </section>
+    <aside></aside>
+    <footer></footer>
+</body>
+</html>
