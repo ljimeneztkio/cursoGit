5ad3ac7ec84efe45aa27c205a4464d34cdded1f5
LuisJimenez <ljimenezt@kio.tech>
2024-07-15 17:59:07 -0600

    Se corrige enlace a la hoja de estilos en la pagina option2.php

--- option2.php
+++ option2.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main/main.css">
+    <link rel="stylesheet" href="css/main.css">
     <title>Opcion 2</title>
 </head>
 <body>
