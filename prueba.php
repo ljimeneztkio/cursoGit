c81222920f7ddbea875204c98dc5d6c2622b4e96
LuisJimenez <ljimenezt@kio.tech>
2024-07-14 20:15:00 -0600

    Se agregan dos divs con contenido lorem en el section

--- prueba.php
+++ prueba.php
@@ -26,6 +26,23 @@
 		<div>
 			<?= "Bienvenido a la página web de esta empresa" ?>
 		</div>
+
+		<div>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure a dignissimos, eos quasi ab soluta quis dolor earum quaerat cupiditate dolorem alias, possimus, esse quidem hic harum? Qui, nam quaerat.
+		</div>
+
+		<div>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur, sequi aspernatur itaque id eligendi alias eveniet deserunt laudantium rem molestias assumenda ipsa delectus tenetur sed odio, ratione velit molestiae?
+			Dolores minima at non, quisquam, obcaecati aperiam unde odio, voluptatem ut eveniet natus architecto laboriosam! Possimus nisi quas praesentium magnam esse facilis maxime, quod nihil recusandae culpa obcaecati quis deleniti?
+			Maxime, quibusdam repellendus autem tenetur quam quo laborum asperiores et molestiae vel reiciendis eius, dignissimos, dolorem dolorum laudantium! Veniam, labore. Debitis, ut placeat expedita dolorem accusantium nulla enim ipsa laudantium.
+			Voluptate molestiae sint mollitia repellat atque? Quo, provident labore. Animi blanditiis voluptas delectus assumenda minima ipsum placeat, qui a! Error nihil quos non molestiae, enim qui reprehenderit maxime doloribus alias.
+			Aperiam ipsum alias impedit nostrum debitis error. Et, a aperiam. Autem ab libero voluptate sequi dolorem id in tempore, velit accusamus impedit iure minus, enim distinctio nulla necessitatibus quisquam incidunt!
+			Officiis expedita qui pariatur soluta beatae recusandae harum aut dolorum similique. Maiores iste facere necessitatibus temporibus, libero odit nisi vel culpa ad, consequuntur id consectetur ab eveniet qui et optio!
+			Dignissimos sint nostrum voluptatum deserunt asperiores distinctio rem ducimus voluptatibus quasi deleniti, quas voluptatem molestiae necessitatibus explicabo quidem! Magni nostrum assumenda iure accusantium neque ipsum nisi itaque officia tempore voluptas.
+			Doloremque corrupti aperiam pariatur, dolorum amet sit minus impedit culpa ex natus voluptatum ipsam reprehenderit, ratione illum est debitis sint doloribus esse laborum ea laudantium atque delectus tenetur. Dolorum, optio.
+			Sint eos sit dolore repellat dicta molestiae adipisci dolorum facilis. Maxime aspernatur vel facilis reprehenderit optio nulla esse atque! Rem illum ipsum dolore, maxime nulla voluptatum obcaecati reiciendis ut unde!
+			Magnam, facere nisi odio quo quam nostrum ipsa perferendis, commodi, atque doloribus fugit voluptate odit quia modi nemo soluta. Vel laborum necessitatibus quasi? Deserunt blanditiis vitae harum necessitatibus voluptatum cumque.
+		</div>
 	</section>
 	<aside>
 		<div>
