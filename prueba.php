ea1d7b9f87b468fcb7611532e31db94e545616df
Luis Enrique Jimenez Trejo <165965316+ljimeneztkio@users.noreply.github.com>
2024-07-16 17:48:52 -0600

    Se cambia texto de Lorem por otro mensaje
    Se elimina el texto largo del Lorem por mensaje de que lindo es usar git

--- prueba.php
+++ prueba.php
@@ -32,18 +32,9 @@
 		</div>
 
 		<div>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur, sequi aspernatur itaque id eligendi alias eveniet deserunt laudantium rem molestias assumenda ipsa delectus tenetur sed odio, ratione velit molestiae?
-			Dolores minima at non, quisquam, obcaecati aperiam unde odio, voluptatem ut eveniet natus architecto laboriosam! Possimus nisi quas praesentium magnam esse facilis maxime, quod nihil recusandae culpa obcaecati quis deleniti?
-			Maxime, quibusdam repellendus autem tenetur quam quo laborum asperiores et molestiae vel reiciendis eius, dignissimos, dolorem dolorum laudantium! Veniam, labore. Debitis, ut placeat expedita dolorem accusantium nulla enim ipsa laudantium.
-			Voluptate molestiae sint mollitia repellat atque? Quo, provident labore. Animi blanditiis voluptas delectus assumenda minima ipsum placeat, qui a! Error nihil quos non molestiae, enim qui reprehenderit maxime doloribus alias.
-			Aperiam ipsum alias impedit nostrum debitis error. Et, a aperiam. Autem ab libero voluptate sequi dolorem id in tempore, velit accusamus impedit iure minus, enim distinctio nulla necessitatibus quisquam incidunt!
-			Officiis expedita qui pariatur soluta beatae recusandae harum aut dolorum similique. Maiores iste facere necessitatibus temporibus, libero odit nisi vel culpa ad, consequuntur id consectetur ab eveniet qui et optio!
-			Dignissimos sint nostrum voluptatum deserunt asperiores distinctio rem ducimus voluptatibus quasi deleniti, quas voluptatem molestiae necessitatibus explicabo quidem! Magni nostrum assumenda iure accusantium neque ipsum nisi itaque officia tempore voluptas.
-			Doloremque corrupti aperiam pariatur, dolorum amet sit minus impedit culpa ex natus voluptatum ipsam reprehenderit, ratione illum est debitis sint doloribus esse laborum ea laudantium atque delectus tenetur. Dolorum, optio.
-			Sint eos sit dolore repellat dicta molestiae adipisci dolorum facilis. Maxime aspernatur vel facilis reprehenderit optio nulla esse atque! Rem illum ipsum dolore, maxime nulla voluptatum obcaecati reiciendis ut unde!
-			Magnam, facere nisi odio quo quam nostrum ipsa perferendis, commodi, atque doloribus fugit voluptate odit quia modi nemo soluta. Vel laborum necessitatibus quasi? Deserunt blanditiis vitae harum necessitatibus voluptatum cumque.
+			
 		</div>
-
+			Que bonito es Usar Git
 		<div>
 			Utilizamos el commando git commit -am para sustituir (git status .) seguido de realizar el commit
 		</div>
